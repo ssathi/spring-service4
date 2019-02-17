8a90b00e192b5245e6cf86cedeb56add7279f6bf
sathi2 <sathi@wired2perform.com>
2019-02-17 12:56:02 +0530

    new repo - ec241f6741178df4d4678d13052aa6402908e5c2

---
+++ src/main/java/com/example/springservice4/SpringService4Application.java
@@ -0,0 +1,13 @@
+package com.example.springservice4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringService4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringService4Application.class, args);
+	}
+
+}
